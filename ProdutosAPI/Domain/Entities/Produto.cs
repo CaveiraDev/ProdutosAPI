8005372d6962c84c1d5048dd760ea450d1af8b6b
Marcos-Alcantara-EM <marcos.alcantara@escolarmanager.com.br>
2025-10-03 16:17:41 -0300

    alterando produto para construtor primario

--- ProdutosAPI/Domain/Entities/Produto.cs
+++ ProdutosAPI/Domain/Entities/Produto.cs
@@ -1,24 +1,15 @@
 ﻿namespace ProdutosAPI.Domain.Entities
 {
-    public class Produto
+    public class Produto(int id, string nome, string categoria, decimal preco, int quantidade)
     {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Categoria { get; set; }
-        public decimal Preco { get; set; }
-        public int Quantidade { get; set; }
+        public int Id { get; set; } = id;
+        public string Nome { get; set; } = nome;
+        public string Categoria { get; set; } = categoria;
+        public decimal Preco { get; set; } = preco;
+        public int Quantidade { get; set; } = quantidade;
         public DateTime DataInclusao { get; } = DateTime.Now;
 
         public bool Disponivel => Quantidade > 0;
-
-        public Produto(int id, string nome, string categoria, decimal preco, int quantidade)
-        {
-            Id = id;
-            Nome = nome;
-            Categoria = categoria;
-            Preco = preco;
-            Quantidade = quantidade;
-        }
 
         public Produto() : this(0, string.Empty, string.Empty, 0m, 0) { }
     }
