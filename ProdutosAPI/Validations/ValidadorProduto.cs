5e881825f2abb462d272786114e0a35a3a994788
Marcos Alcantara <caveiradev@gmail.com>
2025-10-07 23:02:18 -0300

    Separando as regras de validação do atualizar

--- ProdutosAPI/Validations/ValidadorProduto.cs
+++ ProdutosAPI/Validations/ValidadorProduto.cs
@@ -51,7 +51,34 @@ namespace ProdutosAPI.Validations
 
         public override void AssineRegrasAtualizacao()
         {
-            AssineRegrasInclusao();
+            RuleFor(p => p.Nome)
+                .NotEmpty()
+                .WithMessage(CAMPO_NULO_OU_VAZIO)
+                .NotNull()
+                .WithMessage(CAMPO_OBRIGATORIO_NAO_INFORMADO)
+                .MaximumLength(200)
+                .WithMessage("Nome não pode ter mais de 100 caracteres.");
+
+            RuleFor(p => p.Categoria)
+                .NotEmpty()
+                .WithMessage(CAMPO_NULO_OU_VAZIO)
+                .NotNull()
+                .WithMessage(CAMPO_OBRIGATORIO_NAO_INFORMADO)
+                .MaximumLength(100)
+                .WithMessage("Categoria não pode ter mais de 100 caracteres.");
+
+            RuleFor(p => p.Preco)
+                .GreaterThan(0)
+                .WithMessage(CAMPO_DEVE_SER_MAIOR_QUE_ZERO)
+                .LessThanOrEqualTo(999999.99m)
+                .OverridePropertyName("Preço")
+                .WithMessage("Preço não pode ser maior que 999999.99.");
+
+            RuleFor(p => p.Quantidade)
+                .GreaterThanOrEqualTo(0)
+                .WithMessage(CAMPO_DEVE_SER_MAIOR_QUE_ZERO)
+                .LessThanOrEqualTo(100000)
+                .WithMessage("Quantidade não pode ser maior que 100000.");
 
             RuleFor(p => p)
                .Custom((m, context) =>
